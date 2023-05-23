432ae1f340828abadad7b27d24c429bf3667832a
patriciacostadacruz <patriciacstcz@gmail.com>
2023-05-23 16:00:53 +0200

    Adds navbar items without links so far

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <div>
-      <h3>Navbar</h3>
+      <ul>
+        <li>
+          <NavLink to="">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="">Expertise</NavLink>
+        </li>
+        <li>
+          <NavLink to="">Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to="">Work experience</NavLink>
+        </li>
+        <li>
+          <NavLink to="">References</NavLink>
+        </li>
+        <li>
+          <NavLink to="">Contact</NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
