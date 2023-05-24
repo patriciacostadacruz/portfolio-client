aa93a5a4d4d89a20594b5aff3cfbcd9e141bd4c9
patriciacostadacruz <patriciacstcz@gmail.com>
2023-05-24 12:31:18 +0200

    Creates index file to allow a single import to get all images in img folder

---
+++ src/images/index.js
@@ -0,0 +1,28 @@
+// file created to import all images at once in component
+import food1 from './food1.png';
+import food2 from './food2.png';
+import food3 from './food3.png';
+import hubble1 from './hubble1.png';
+import hubble2 from './hubble2.png';
+import hubble3 from './hubble3.png';
+import investmate1 from './investmate1.png';
+import investmate2 from './investmate2.png';
+import investmate3 from './investmate3.png';
+import ADPlogo from './adp-logo.png';
+import interrogationPoint from './interrogation-point.png';
+
+const images = {
+  food1,
+  food2,
+  food3,
+  hubble1,
+  hubble2,
+  hubble3,
+  investmate1,
+  investmate2,
+  investmate3,
+  ADPlogo,
+  interrogationPoint,
+};
+
+export default images;
