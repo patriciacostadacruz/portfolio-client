9d70ea368a82e214f5f49e6cbd9f21e14a0a7cec
patriciacostadacruz <patriciacstcz@gmail.com>
2023-06-09 12:49:02 +0200

    Adds logic to show navbar menu when screen is smaller than 900px width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, animateScroll as scroll } from 'react-scroll';
 import { toast } from 'react-hot-toast';
 import laptop from '../images/laptop.png';
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const [isScreenWide, setIsScreenWide] = useState(window.innerWidth > 900);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsScreenWide(window.innerWidth > 900);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const handleScrollToTop = () => {
     scroll.scrollToTop({
       // acceleration until halfway, then deceleration
@@ -18,81 +31,92 @@ export default function Navbar() {
     });
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="navbar-container">
-      <ul className="navbar-items">
-        <img src={laptop} alt="Laptop icon" onClick={handleClick} />
-        <li>
-          <button className="navbar-button" onClick={handleScrollToTop}>
-            // home
-          </button>
-        </li>
-        <li>
-          <Link
-            className="navbar-options"
-            activeClass="active"
-            to="expertise"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={1000}
-          >
-            // expertise
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="navbar-options"
-            activeClass="active"
-            to="projects"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={1000}
-          >
-            // projects
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="navbar-options"
-            activeClass="active"
-            to="experience"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={1000}
-          >
-            // work experience
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="navbar-options"
-            activeClass="active"
-            to="references"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={1000}
-          >
-            // references
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="navbar-options"
-            activeClass="active"
-            to="contact"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={1000}
-          >
-            // contact
-          </Link>
-        </li>
-      </ul>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      {(isMenuOpen || isScreenWide) && (
+        <ul className="navbar-items">
+          <img src={laptop} alt="Laptop icon" onClick={handleClick} />
+          <li>
+            <button className="navbar-button" onClick={handleScrollToTop}>
+              // HOME
+            </button>
+          </li>
+          <li>
+            <Link
+              className="navbar-options"
+              activeClass="active"
+              to="expertise"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={1000}
+            >
+              // EXPERTISE
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="navbar-options"
+              activeClass="active"
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={1000}
+            >
+              // PROJECTS
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="navbar-options"
+              activeClass="active"
+              to="experience"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={1000}
+            >
+              // WORK EXPERIENCE
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="navbar-options"
+              activeClass="active"
+              to="references"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={1000}
+            >
+              // REFERENCES
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="navbar-options"
+              activeClass="active"
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={1000}
+            >
+              // CONTACT
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 }
